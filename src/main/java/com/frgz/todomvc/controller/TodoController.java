4bb7a1171a2f1d5df61f2c487e379acb9dcdf9dd
Fabio Ramon Dieguez Perez <fabiordp@ext.inditex.com>
2021-02-09 23:30:42 +0100

    Create and delete all

--- src/main/java/com/frgz/todomvc/controller/TodoController.java
+++ src/main/java/com/frgz/todomvc/controller/TodoController.java
@@ -4,15 +4,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.frgz.todomvc.domain.Todo;
 import com.frgz.todomvc.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.OPTIONS;
+import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@CrossOrigin(
+    methods ={ POST, GET, OPTIONS, DELETE, PATCH},
+    maxAge =3600,
+    allowedHeaders = { "x-requested-with", "origin", "content-type", "accept", "accept-patch"},
+    origins = "*"
+)
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/todo")
@@ -24,6 +43,18 @@ public class TodoController {
   public ResponseEntity<List<Todo>> find() {
     final List<Todo> todos = StreamSupport.stream(this.todoRepository.findAll().spliterator(), false).collect(Collectors.toList());
     return ResponseEntity.ok(todos);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Todo create(@RequestBody final Todo todo) {
+    return this.todoRepository.save(todo);
+  }
+
+  @DeleteMapping
+  @ResponseStatus(HttpStatus.OK)
+  public void deleteAll() {
+    todoRepository.deleteAll();
   }
 
 }
--- src/test/java/com/frgz/todomvc/controller/TodoControllerTest.java
+++ src/test/java/com/frgz/todomvc/controller/TodoControllerTest.java
@@ -1,17 +1,29 @@
 package com.frgz.todomvc.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.frgz.todomvc.domain.Todo;
 import com.frgz.todomvc.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,6 +40,27 @@ public class TodoControllerTest {
     mvc.perform(get("/api/todo")) //
         .andExpect(status().isOk()) //
         .andExpect(content().string("[]"));
+  }
+
+  @Test
+  public void testGivenAJSONPayloadShouldSaveATodo() throws Exception {
+    final Todo todo = Todo.builder().title("Grab your hat").build();
+
+    given(todoRepository.save(any(Todo.class))).willReturn(todo);
+
+    mvc.perform(post("/api/todo")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"title\":\"Grab your hat\"}".getBytes())
+        .characterEncoding("utf-8"))
+        .andExpect(status().is(HttpStatus.CREATED.value()))
+        .andExpect(jsonPath("$.title", is("Grab your hat")));
+  }
+
+  @Test
+  public void testDeleteAllTodos() throws Exception {
+    mvc.perform(delete("/api/todo")).andExpect(status().isOk());
+
+    verify(todoRepository, atLeast(1)).deleteAll();
   }
 
 }
